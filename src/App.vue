678603d93fd82775dec43716d7d8b3e232ab26e7
skjalosc <oscarskjal2@gmail.com>
2024-09-26 09:58:44 +0300

    Nya main layouten

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Question from "./components/Question.vue";
+
+const text = "does this work";
+</script>
+
+<template>
+  <div id="app">
+    <Header></Header>
+    <Question></Question>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Learning app</h1>
+</template>
+
+<style scoped></style>
