0b7cebb2d93dcffa5bf97b0523d2f1f4a8b1a119
skjalosc <oscarskjal2@gmail.com>
2024-09-26 16:08:53 +0300

    backend koppling ok

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
-<script setup>
-import Header from "./components/Header.vue";
-import Question from "./components/Question.vue";
-
-const text = "does this work";
-</script>
-
 <template>
   <div id="app">
-    <Header></Header>
-    <Question></Question>
+    <Navigation></Navigation>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<style scoped></style>
+<script>
+import Navigation from "./components/Navigation.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  components: {
+    Navigation,
+  },
+  mounted() {
+    axios
+      .get("http://localhost:5000/api/hello")
+      .then((response) => {
+        this.message = response.data.message;
+      })
+      .catch((error) => {
+        console.error("Error fetching message:", error);
+      });
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:5000/api/hello")
+      .then((response) => {
+        this.message = response.data.message;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
